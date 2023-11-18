fb9f1933683b6897d2288f6ce46b43f1369fb0ac
Shreyansh Jain <145953907+iam-shrey@users.noreply.github.com>
2023-11-18 19:15:33 +0530

    Add files via upload

---
+++ gradecalculator.java
@@ -0,0 +1,34 @@
+package projects;
+
+import java.util.*;
+
+public class gradecalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number of Subjects You Have in Total: ");
+        int n = sc.nextInt();
+        float marks=0;
+        System.out.println("Enter the Marks Obtained(out of 100) in Each Subjects One by One: ");
+        for(int i=0;i<n;i++){
+            marks+=sc.nextFloat();
+        }
+        float avg = marks/n;
+        System.out.println("Total Marks Obtained = "+marks+"/"+n*100);
+        System.out.printf("Average Percentage = %.2f",avg);System.out.println(" %");
+        if(avg>=90)
+            System.out.println("Grade Obtained = A+");
+        else if(avg>=80 && avg<90)
+            System.out.println("Grade Obtained = A");
+        else if(avg>=70 && avg<80)
+            System.out.println("Grade Obtained = B+");
+        else if(avg>=60 && avg<70)
+            System.out.println("Grade Obtained = B");
+        else if(avg>=50 && avg<60)
+            System.out.println("Grade Obtained = C+");
+        else if(avg>=40 && avg<50)
+            System.out.println("Grade Obtained = C");
+        else if(avg>=33 && avg<40)
+            System.out.println("Grade Obtained = D");
+        sc.close();
+    }
+}
